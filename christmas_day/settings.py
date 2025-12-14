907671ceac2df39a65a3c3fed0c915a966cc89fb
Ben Noyes <noyesbenjamin@gmail.com>
2025-12-14 09:30:01 -0500

    staticfiles

--- christmas_day/settings.py
+++ christmas_day/settings.py
@@ -202,7 +202,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # will need to change this in deployment to S3
 MEDIA_URL = "/media/"
