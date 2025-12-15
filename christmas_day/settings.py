3360a3fd2cbec480cde4e947787f30a8aea404e8
Ben Noyes <noyesbenjamin@gmail.com>
2025-12-15 09:34:16 -0500

    added api url to cors_allowed_origins

--- christmas_day/settings.py
+++ christmas_day/settings.py
@@ -80,7 +80,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://54.215.246.62",
     "http://thankyounotes.today",
     "http://www.thankyounotes.today",
-    "https://main.dld367td3v4c7.amplifyapp.com"
+    "https://api.thankyounotes.today"
 ]
 
 CORS_ALLOW_CREDENTIALS = True # cookie settings
