9873e4b7983b9da6d8c01a8eb5777cd63c075840
Ben Noyes <noyesbenjamin@gmail.com>
2025-12-14 19:17:57 -0500

    urls issue

--- sm/urls.py
+++ sm/urls.py
@@ -7,6 +7,7 @@ favicon_view = RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"), 
 
 urlpatterns = [
     # user management
+    path("", RedirectView.as_view(url="/register_user/", permanent=False)),
     path("register_user/", views.register_user, name="register_user"),
     path("update_user/", views.update_user, name="update_user"),
     path("my_profile/", views.get_current_user, name="profile"),
