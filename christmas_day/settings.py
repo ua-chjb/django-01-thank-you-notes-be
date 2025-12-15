1c9b9ec0ea019a16cea1afc71c2f3b06b51de4fc
Ben Noyes <noyesbenjamin@gmail.com>
2025-12-15 15:28:19 -0500

    tuned aws parameters

--- christmas_day/settings.py
+++ christmas_day/settings.py
@@ -220,7 +220,7 @@ AWS_SECRET_ACCESS_KEY = ssm.get_parameter(Name="S3_SECRET_KEY", WithDecryption=T
 
 AWS_STORAGE_BUCKET_NAME = "django-01-tyn-s3"
 AWS_S3_REGION_NAME = "us-west-1"
-AWS_DEFAULT_ACL = "public-read"
+AWS_DEFAULT_ACL = None
 AWS_S3_FILE_OVERWRITE = False
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
